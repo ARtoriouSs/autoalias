0f90d23460a483e9a88e179669eb9554ccd50626
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-04 11:12:20 +0300

    Add basic conflicts filtering

--- lib/autoalias.ex
+++ lib/autoalias.ex
@@ -1,14 +1,53 @@
 defmodule Autoalias do
   @moduledoc """
-  Documentation for Autoalias.
+  TODO: Documentation for Autoalias
   """
 
+  @doc "TODO"
   defmacro __using__(_args) do
-    :code.all_loaded
+    #:code.all_loaded
+    [{Foo.Bar.Keks, 'asd'}, {Lel.Keks, ''}]
     |> Enum.map(fn {module, _path} -> module end)
     |> Enum.filter(&elixir_module?/1)
+    |> resolve_conflicts
     |> Enum.map(&quote_alias/1)
     |> quote_block
+  end
+
+  defp elixir_module?(module) do
+    match?("Elixir." <> _, Atom.to_string(module))
+  end
+
+  defp resolve_conflicts(modules) do
+    keks =
+      modules
+      |> Enum.map(fn module -> get_conflicts(module, modules) end)
+
+          require IEx; IEx.pry
+
+
+  end
+
+  defp get_conflicts(target, modules) do
+    conflicts =
+      modules
+      |> Enum.map(fn module ->
+        conflicts = if module != target and last_child(module) == last_child(target) do
+          module
+        else
+          nil
+        end
+      end)
+      |> Enum.reject(&is_nil/1)
+
+    [target: target, conflicts: conflicts]
+  end
+
+  defp last_child(module) do
+    module
+    |> Atom.to_string
+    |> String.split(".")
+    |> Enum.take(-1)
   end
 
   defp quote_alias(module) do
@@ -20,8 +59,4 @@ defmodule Autoalias do
   end
 
   defp quote_block(aliases), do: {:__block__, [], aliases}
-
-  defp elixir_module?(module) do
-    match?("Elixir." <> _, Atom.to_string(module))
-  end
 end
