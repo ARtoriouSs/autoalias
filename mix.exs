93f89c2a3c28c815472d6b3adf18fe4f6e27284e
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-05 02:13:54 +0300

    Add ex_docs

--- mix.exs
+++ mix.exs
@@ -37,6 +37,8 @@ defmodule Autoalias.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 end
