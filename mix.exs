475f505cac2bd4b1ffa5a7e4e6a89b7e97f43f85
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-05 02:11:19 +0300

    Add description and metadata for hex

--- mix.exs
+++ mix.exs
@@ -7,8 +7,25 @@ defmodule Autoalias.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Alexander Teslovskiy"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ARtoriouSs/autoalias"}
+    ]
+  end
+
+  defp description do
+    """
+    TODO
+    """
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -20,9 +37,6 @@ defmodule Autoalias.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
